c788793d4e9d4d6fc334617888dc49b4080a6b97
VISWESWARAN1998 <visweswaran.nagasivam98@gmail.com>
2017-12-30 16:44:11 +0530

    ReadMe v0.7 :green_book:

---
+++ examples/udp_client.cpp
@@ -0,0 +1,20 @@
+// SWAMI KARUPPASWAMI THUNNAI
+
+#include<iostream>
+#include"client_socket.h"
+
+int main()
+{
+	try {
+		client_socket::udp client("127.0.0.1", 90);
+		sockaddr_in from;
+		client.get_client_socketaddr(from);
+		client.send_message(from, "Hello There");
+		std::cout<<client.receive(11, from);
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "exception: ";
+		std::cout << e.what();
+	}
+}
